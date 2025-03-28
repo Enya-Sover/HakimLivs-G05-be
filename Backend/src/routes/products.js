522a1d01226e13a18c61ac6a32b04f9f1efcde2f
Enya-Sover <David.kagell@yh.nackademin.se>
2025-03-28 12:42:34 +0100

    småfix

--- Backend/src/routes/products.js
+++ Backend/src/routes/products.js
@@ -1,16 +1,10 @@
 import express from "express";
-import { adminAuth } from "../middleware/auth.js";
 import { getAllProducts, deleteProduct, createProducts, updateProduct } from "../controllers/productController.js";
 
 const productsRouter = express.Router();
-
 productsRouter.get("/", getAllProducts);
-
-// Create product (admin only)
 productsRouter.post("/", createProducts)
-
 productsRouter.delete('/:id', deleteProduct)
-
 productsRouter.put('/:id', updateProduct)
 
 
