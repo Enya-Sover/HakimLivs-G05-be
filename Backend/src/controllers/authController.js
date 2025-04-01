3a2f6a1da44b2fa12fe4903ce46e68e3815460f3
Enya-Sover <David.kagell@yh.nackademin.se>
2025-04-01 11:37:45 +0200

    småfix

---
+++ Backend/src/controllers/authController.js
@@ -0,0 +1,48 @@
+
+import jwt from 'jsonwebtoken';
+import User from '../models/User.js';
+
+export const register = async (req, res) => {
+    try {
+      const user = new User(req.body);
+      await user.save();
+      
+      const token = jwt.sign(
+        { id: user._id, isAdmin: user.isAdmin },
+        process.env.ACCESS_TOKEN_SECRET, {expiresIn: process.env.ACCESS_TOKEN_EXPIRY}
+      );
+      
+      res.status(201).json({ user, token });
+    } catch (error) {
+      res.status(400).json({ error: error.message });
+    }
+  }
+
+  export const login =  async (req,res)=>{
+
+    try {
+      const {username, password} = req.body
+      const user = await User.findOne({username})
+      if(!user){
+        return res.status(401).json({error: 'Fel användarnamn eller lösenord'})
+      }
+  
+      const isMatch = await user.comparePassword(password)
+      if(!isMatch){
+        return res.status(401).json({error: 'Fel användarnamn eller lösenord'})
+      }
+      const accessToken = jwt.sign(
+        { id: user._id, isAdmin: user.isAdmin },
+        process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: process.env.ACCESS_TOKEN_EXPIRY }
+      );
+      const refreshToken = jwt.sign(
+        { id: user._id, isAdmin: user.isAdmin },
+        process.env.REFRESH_TOKEN_SECRET,
+        { expiresIn: process.env.REFRESH_TOKEN_EXPIRY }
+      );
+      res.json({message: 'Inloggningen lyckades', accessToken, refreshToken})
+    } catch (error) {
+      res.status(500).json({error: error.message})
+    }
+  }
--- Backend/src/routes/auth.js
+++ Backend/src/routes/auth.js
@@ -1,54 +1,12 @@
 import express from 'express';
-import jwt from 'jsonwebtoken';
-import User from '../models/User.js';
+import {register, login} from '../controllers/authController.js'
 
 const authRouter = express.Router();
 
 // Register
-authRouter.post('/register', async (req, res) => {
-  try {
-    const user = new User(req.body);
-    await user.save();
-    
-    const token = jwt.sign(
-      { id: user._id, isAdmin: user.isAdmin },
-      process.env.ACCESS_TOKEN_SECRET, {expiresIn: process.env.ACCESS_TOKEN_EXPIRY}
-    );
-    
-    res.status(201).json({ user, token });
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-});
+authRouter.post('/register', register);
 
 //TODO Login
-authRouter.post('/login', async (req,res)=>{
-
-  try {
-    const {username, password} = req.body
-    const user = await User.findOne({username})
-    if(!user){
-      return res.status(401).json({error: 'Fel användarnamn eller lösenord'})
-    }
-
-    const isMatch = await user.comparePassword(password)
-    if(!isMatch){
-      return res.status(401).json({error: 'Fel användarnamn eller lösenord'})
-    }
-    const accessToken = jwt.sign(
-      { id: user._id, isAdmin: user.isAdmin },
-      process.env.ACCESS_TOKEN_SECRET,
-      { expiresIn: process.env.ACCESS_TOKEN_EXPIRY }
-    );
-    const refreshToken = jwt.sign(
-      { id: user._id, isAdmin: user.isAdmin },
-      process.env.REFRESH_TOKEN_SECRET,
-      { expiresIn: process.env.REFRESH_TOKEN_EXPIRY }
-    );
-    res.json({message: 'Inloggningen lyckades', accessToken, refreshToken})
-  } catch (error) {
-    res.status(500).json({error: error.message})
-  }
-})
+authRouter.post('/login', login)
 
 export default authRouter;
