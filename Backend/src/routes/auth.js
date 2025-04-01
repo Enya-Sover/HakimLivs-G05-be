b8b4e84181f3ebb1132adf261a9bfe339df35359
Enya-Sover <David.kagell@yh.nackademin.se>
2025-04-01 11:30:43 +0200

    jobbar med JWT och auth

--- Backend/src/routes/auth.js
+++ Backend/src/routes/auth.js
@@ -2,17 +2,17 @@ import express from 'express';
 import jwt from 'jsonwebtoken';
 import User from '../models/User.js';
 
-const router = express.Router();
+const authRouter = express.Router();
 
 // Register
-router.post('/register', async (req, res) => {
+authRouter.post('/register', async (req, res) => {
   try {
     const user = new User(req.body);
     await user.save();
     
     const token = jwt.sign(
       { id: user._id, isAdmin: user.isAdmin },
-      process.env.JWT_SECRET || 'your-secret-key'
+      process.env.ACCESS_TOKEN_SECRET, {expiresIn: process.env.ACCESS_TOKEN_EXPIRY}
     );
     
     res.status(201).json({ user, token });
@@ -22,5 +22,33 @@ router.post('/register', async (req, res) => {
 });
 
 //TODO Login
+authRouter.post('/login', async (req,res)=>{
 
-export default router;
+  try {
+    const {username, password} = req.body
+    const user = await User.findOne({username})
+    if(!user){
+      return res.status(401).json({error: 'Fel användarnamn eller lösenord'})
+    }
+
+    const isMatch = await user.comparePassword(password)
+    if(!isMatch){
+      return res.status(401).json({error: 'Fel användarnamn eller lösenord'})
+    }
+    const accessToken = jwt.sign(
+      { id: user._id, isAdmin: user.isAdmin },
+      process.env.ACCESS_TOKEN_SECRET,
+      { expiresIn: process.env.ACCESS_TOKEN_EXPIRY }
+    );
+    const refreshToken = jwt.sign(
+      { id: user._id, isAdmin: user.isAdmin },
+      process.env.REFRESH_TOKEN_SECRET,
+      { expiresIn: process.env.REFRESH_TOKEN_EXPIRY }
+    );
+    res.json({message: 'Inloggningen lyckades', accessToken, refreshToken})
+  } catch (error) {
+    res.status(500).json({error: error.message})
+  }
+})
+
+export default authRouter;
