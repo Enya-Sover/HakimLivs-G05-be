3e6d01005ff9e4ba899a5e355f9eacbb3e428c8c
Enya-Sover <David.kagell@yh.nackademin.se>
2025-04-07 15:56:43 +0200

    småfix

--- Backend/src/routes/auth.js
+++ Backend/src/routes/auth.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import {getAdminUser, register, login} from '../controllers/authController.js'
+import { register, login} from '../controllers/authController.js'
 
 const authRouter = express.Router();
 
