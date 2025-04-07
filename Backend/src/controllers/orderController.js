54a11e0453f57c8725eb66589a66dd030b3f700c
ErikBergloef <47384098+ErikBergloef@users.noreply.github.com>
2025-04-07 16:14:24 +0200

    heh

--- Backend/src/controllers/orderController.js
+++ Backend/src/controllers/orderController.js
@@ -1,5 +1,6 @@
 import Order from "../models/Order.js";
 import mongoose from "mongoose";
+import User from "../models/User.js";
 
 export const getAllOrders = async (req, res) => {
     try {
@@ -22,5 +23,20 @@ export const getOrderById = async (req, res) => {
         }
     } catch (error) {
         res.status(500).json({ error: error.message })
+    }
+}
+
+export const getUserOrders = async (req, res) => {
+    try {
+        const userId = req.user._id;
+        const orders = await Order.find({user: userId }).populate('items.productId')
+
+        if (orders.length === 0) {
+            return res.status(404).json({ message: 'Ingen orderhistorik finns kopplad till denna kund' })
+        }
+        res.status(200).json(orders);
+    } catch { 
+        res.status(500).json({ error: 'Serverfel', details: error.message });
+
     }
 }
--- Backend/src/routes/users.js
+++ Backend/src/routes/users.js
@@ -1,12 +1,11 @@
 import express from 'express'
 import {getAllUsers} from '../controllers/userController.js'
-// import authMiddleware from '../middleware/authMiddleware.js';
+import { getUserOrders } from '../controllers/orderController.js';
 
 const userRoutes = express.Router();
 
 userRoutes.get("/", getAllUsers)
-
-
+userRoutes.get("/orders", getUserOrders) //api/user/orders
 
 export default userRoutes
 
