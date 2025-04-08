b2a5d87d0df745e407c3e288f10e7c8ca48892be
Enya-Sover <David.kagell@yh.nackademin.se>
2025-04-08 15:16:15 +0200

    testar enligt Jonatans kod

--- Backend/src/middleware/auth.js
+++ Backend/src/middleware/auth.js
@@ -2,13 +2,13 @@ import jwt from 'jsonwebtoken';
 
 
 export const auth = async (req, res, next) => {
-  const authHeader = req.headers.authorization
-  // implementera lite "misstagslogik"
-  if (!authHeader || !authHeader.startsWith('Bearer')) {
-    return res.status(401).json({ message: 'Ingen token tillhandahållen' });
-  }
-  const token = authHeader.split(' ')[1];
   try {
+    const authHeader = req.headers['authorization']
+    // implementera lite "misstagslogik"
+    if (!authHeader || !authHeader.startsWith('Bearer')) {
+      return res.status(401).json({ message: 'No token available' });
+    }
+    const token = authHeader.split(' ')[1];
     const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
     req.user = decoded;
     next();
