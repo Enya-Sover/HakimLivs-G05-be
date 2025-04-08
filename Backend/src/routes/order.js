2d423dc1fd4dc6173e12e756a24d02b15e96bd97
Felix0291 <felix.tapper@gmail.com>
2025-04-08 10:53:00 +0200

    getUserOrders route

--- Backend/src/routes/order.js
+++ Backend/src/routes/order.js
@@ -1,11 +1,13 @@
 import express from 'express'
-import { getAllOrders, getOrderById, createNewOrder } from '../controllers/orderController.js'
+import { getAllOrders, getOrderById, getUserOrders, createNewOrder } from '../controllers/orderController.js'
 
 const orderRoutes = express.Router()
 
 
 orderRoutes.get("/", getAllOrders)
 orderRoutes.get("/:id", getOrderById )
+orderRoutes.get("/orderHistory", getUserOrders) //api/order/orderHistory
 orderRoutes.post("/", createNewOrder)
+
 
 export default orderRoutes
