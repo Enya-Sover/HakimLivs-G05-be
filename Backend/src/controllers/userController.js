64b726ef0e2e0deafcb332cd0c75a34d37c9d63c
Felix0291 <felix.tapper@gmail.com>
2025-04-09 12:48:15 +0200

    async function getUserById + updatePassword

--- Backend/src/controllers/userController.js
+++ Backend/src/controllers/userController.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import User from "../models/User.js";
+import bcrypt from "bcryptjs";
 
 
 // Hämta alla användare
@@ -12,4 +13,39 @@ export const getAllUsers = async (req, res) => {
   }
 };
 
+export const getUserById = async (req, res) => {
+  const {id} = req.params
+  try {
+    const user = await User.findById(id);
+    if (user === null) {
+      return res.status(404).json({error: error.message })
+    } else {
+    res.status(200).json(user); 
+  }   
+  } catch (error) {
+    res.status(500).json({ error: error.message })
+  }
+}
 
+export const updatePassword = async(req, res) => {
+  const {id} = req.params
+  const {oldPassword, newPassword} = req.body
+
+  try {
+      const user = await User.findById(id)
+      if (!user) return res.status(400).json({message: "User not found"})
+
+      const isMatch = await bcrypt.compare(oldPassword, user.password)
+      if (!isMatch) return res.status(400).json({message: "Wrong password"})
+      
+        user.password = newPassword
+
+      await user.save()
+
+      res.status(200).json({message: "Uppdated to new passswod"})
+  } catch (err) {
+    console.log(err)
+    res.status(500).json({message: "Something went wrong!"})
+  }
+  
+}
--- Backend/src/routes/users.js
+++ Backend/src/routes/users.js
@@ -1,11 +1,13 @@
 import express from 'express'
-import {getAllUsers} from '../controllers/userController.js'
+import {getAllUsers, getUserById, updatePassword} from '../controllers/userController.js'
 import { getUserOrders } from '../controllers/orderController.js';
-
+import { auth, adminAuth } from '../middleware/auth.js';
 const userRoutes = express.Router();
 
 userRoutes.get("/", getAllUsers)
+userRoutes.get("/:id", getUserById)
 userRoutes.get("/orders", getUserOrders) 
+userRoutes.put("/:id/password",  updatePassword )
 
 export default userRoutes
 
