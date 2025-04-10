30a894900dbe87fb54e70a6a175c9edf0dfd702d
Felix0291 <felix.tapper@gmail.com>
2025-04-10 14:57:44 +0200

    updateUser .put

--- Backend/src/routes/users.js
+++ Backend/src/routes/users.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import {getAllUsers, getUserById, updatePassword} from '../controllers/userController.js'
+import {getAllUsers, getUserById, updatePassword, updateUser} from '../controllers/userController.js'
 import { getUserOrders } from '../controllers/orderController.js';
 import { auth, adminAuth } from '../middleware/auth.js';
 const userRoutes = express.Router();
@@ -8,6 +8,7 @@ userRoutes.get("/", getAllUsers)
 userRoutes.get("/:id", getUserById)
 userRoutes.get("/orders", getUserOrders) 
 userRoutes.put("/:id/password", updatePassword)
+userRoutes.put("/:id/updateUser", updateUser)
 
 export default userRoutes
 
